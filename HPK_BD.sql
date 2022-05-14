44a117b814d329ad27285aaff2c6c235d11a6ef3
Jorge-Osuna <84940374+Jorge-Osuna@users.noreply.github.com>
2022-05-13 21:46:29 -0700

    Create HPK_BD.sql

---
+++ HPK_BD.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE HIGHPERKAI_BD
+USE HIGHPERKAI_BD
+
+CREATE TABLE Receta 
+(
+Nombre varchar(50),
+Calorias decimal,
+Grasas decimal,
+Tiempo int,
+Ingredientes varchar(500),
+Descripcion varchar(500)
+)
